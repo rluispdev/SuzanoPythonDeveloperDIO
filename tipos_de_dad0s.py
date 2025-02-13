db4a4ffd2507d8e4d441d4b2afcbbe149b0b8df7
rluispdev <rluis@icloud.com>
2025-02-13 16:00:39 -0300

    introducao, tipos de dados

---
+++ tipos_de_dad0s.py
@@ -0,0 +1,4 @@
+print(10+34+6)
+print(1.5+1+2.5)
+print(True)
+print("False")
---
+++ variavies_constantes.py
@@ -0,0 +1,12 @@
+nome = "rluispdev"
+idade = 33
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+#Constantes
+STATES = ["SP”, “MG", "RJ", "ES"]
+
+print(STATES)
+
