b9aec93f3e252ae9a777fecf1af1fb39202ae088
rluispdev <rluis@icloud.com>
2025-02-14 14:59:59 -0300

    Operadores aritimeticos

---
+++ operadores_aritimeticos.py
@@ -0,0 +1,16 @@
+produto1 = 10
+produto2 = 20
+
+print(produto1 + produto2)
+print(produto1 - produto2)
+print(produto1 * produto2)
+print(produto1 / produto2)
+print(produto1 // produto2)
+print(produto1 % produto2)
+print(produto1 ** produto2)
+
+x = 10 + 5  * 2
+print(x)
+
+y = (10 + 5) * 2
+print(y)
