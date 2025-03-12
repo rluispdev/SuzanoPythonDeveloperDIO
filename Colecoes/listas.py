b0d60608fd782b5bfc00f66930a9858d28119db3
rluispdev <rluis@icloud.com>
2025-03-12 15:18:16 -0300

    Adicionando Colecoes ao repositório principal

---
+++ Colecoes/listas.py
@@ -0,0 +1,26 @@
+frutas = [ "laranja", "maçã", "uva", "pera", "mamão" ]
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list("Python")
+print(letras)
+
+numeros = list(range(1, 11))
+print(numeros)
+
+carro = ["Ferrari", "F8", 34000, 2020, 2900, "São Paulo", True ]
+print(carro)
+
+
+
+numeros = [ 1, 30, 21, 2, 9, 65, 34]
+
+pares = []
+
+for numero in numeros:
+    if numero % 2 == 0:
+        pares.append(numero)
+
+print(pares)
---
+++ Colecoes/set.py
@@ -0,0 +1,4 @@
+conjuntto = set([1, 2, 3, 4, 5, 5, 6, 7, 8, 8, 9, 10])
+print(conjuntto)
+ 
+ 
